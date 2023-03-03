21764c678e4384bd563aeacb1011fc7dde212d32
danilo-cupido <danilocupido@gmail.com>
2023-03-03 22:53:10 +0000

    define Book type

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import { db } from './connection';
 import { doc, getDoc } from 'firebase/firestore';
 
 function App() {
+	interface Book {
+		title: string;
+	}
+
+	const [data, setData] = useState<Book>();
 	const handleClick = async () => {
-		const docRef = doc(db, 'cities', 'SF');
+		const docRef = doc(db, 'books', 'rrSkXSiaP94CEZ3nfqvw');
 		const docSnap = await getDoc(docRef);
 
 		if (docSnap.exists()) {
-			console.log('Document data:', docSnap.data());
+			const book = docSnap.data() as Book;
+			if (book) {
+				setData(book);
+			}
 		} else {
 			// doc.data() will be undefined in this case
 			console.log('No such document!');
@@ -22,6 +30,7 @@ function App() {
 				Hello world!
 			</h1>
 			<button onClick={handleClick}>Click Me</button>
+			<div className='bg-amber-300'>{data?.title}</div>
 		</div>
 	);
 }

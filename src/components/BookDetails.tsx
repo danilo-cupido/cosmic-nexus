48697d557c57180dbf7283102bb77077e6ba1c2f
danilo-cupido <danilocupido@gmail.com>
2023-03-08 23:18:04 +0000

    style book details page

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -21,40 +21,49 @@ const BookDetails = () => {
 		fetchBook();
 	}, []);
 	return (
-		<div>
+		<div className='flex justify-center'>
 			{book && (
-				<div>
-					{book.imageLinks ? (
-						<img
-							src={book.imageLinks.thumbnail}
-							className='h-32 flex-none bg-cover rounded-t text-center overflow-hidden mr-2 shadow-sm'
-							alt={book.title}
-						/>
-					) : (
-						<div className='flex flex-col h-32 w-24 bg-gray-100 mr-2 justify-center items-center rounded shadow-sm'>
-							<BsImages className='h-12 w-6 text-gray-400 ' />
-							<p className='text-xs text-gray-400 text-center'>
-								No image available
-							</p>
+				<div className='border rounded-md p-10 max-w-screen-lg'>
+					<div className='flex'>
+						{book.imageLinks ? (
+							<img
+								src={book.imageLinks.thumbnail}
+								className='h-46 pr-4 flex-none bg-cover rounded-t text-center overflow-hidden mr-2 shadow-sm'
+								alt={book.title}
+							/>
+						) : (
+							<div className='flex flex-col h-32 w-24 bg-gray-100 mr-2 justify-center items-center rounded shadow-sm'>
+								<BsImages className='h-12 w-6 text-gray-400 ' />
+								<p className='text-xs text-gray-400 text-center'>
+									No image available
+								</p>
+							</div>
+						)}
+						<div className='gap-4'>
+							<p className='text-xl font-bold pb-4'>{book.title}</p>
+							{book.authors ? (
+								<p>
+									{book.authors.length > 1 ? 'Authors: ' : 'Author: '}{' '}
+									{book.authors.join(', ')}
+								</p>
+							) : (
+								<p>Author: N/A</p>
+							)}
+							<div>
+								<p>Publisher: {book.publisher}</p>
+								<p>
+									Publish date:{' '}
+									{book.publishedDate.split('-').reverse().join('-')}
+								</p>
+								<p>Language: {book.language}</p>
+								<p>Pages: {book.pageCount}</p>
+							</div>
 						</div>
-					)}
-					<p>{book.title}</p>
-					{book.authors ? (
-						<p>
-							{book.authors.length > 1 ? 'Authors: ' : 'Author: '}{' '}
-							{book.authors.join(', ')}
-						</p>
-					) : (
-						<p>Author: N/A</p>
-					)}
-					<p>
-						Publish date: {book.publishedDate.split('-').reverse().join('-')}
-					</p>
-					<p>Description: </p>{' '}
-					<div dangerouslySetInnerHTML={{ __html: book.description }}></div>
-					<p>Pages: {book.pageCount}</p>
-					<p>Language: {book.language}</p>
-					<p>Publisher: {book.publisher}</p>
+					</div>
+					<div
+						dangerouslySetInnerHTML={{ __html: book.description }}
+						className='mt-6'
+					></div>
 				</div>
 			)}
 		</div>

e75c53b311b30c3c9aa394915d5944f24916071e
danilo-cupido <danilocupido@gmail.com>
2023-03-23 22:15:08 +0000

    add basic style to ReviewCard

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -3,12 +3,21 @@ import { ReviewData } from '../utils/types';
 
 const ReviewCard = (props: { review: ReviewData }) => {
 	return (
-		<div key={props.review.username} className='flex'>
-			<p>{props.review.username}</p>
-			<p>{props.review.title}</p>
+		<div
+			key={props.review.username}
+			className='flex flex-col border p-6 rounded shadow-sm my-4'
+		>
+			<p className='text-gray-900 font-bold text-md mb-1'>
+				{props.review.username}
+			</p>
+			<p className='text-gray-900 font-bold text-md mb-1'>
+				{props.review.title}
+			</p>
 			<p>{props.review.rate}</p>
 			<p>{props.review.comment}</p>
-			<p>{formattedDate(props.review.createdAt.toDate().toDateString())}</p>
+			<p className='text-gray-700 text-sm'>
+				{formattedDate(props.review.createdAt.toDate().toDateString())}
+			</p>
 		</div>
 	);
 };
